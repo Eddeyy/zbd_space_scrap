5f64b662da9ea59587f25a95df781e8e40133085
Szymon Ludwiniak <259215@student.pwr.edu.pl>
2024-06-02 18:59:43 +0200

    problematic tables :(

--- changelog/ddl/generate.sql
+++ changelog/ddl/generate.sql
@@ -22,14 +22,14 @@ CREATE TABLE Ship (
   PRIMARY KEY (ID));
 CREATE TABLE Scrap (
   ID            BIGSERIAL NOT NULL, 
-  Name          varchar(16), 
+  Name          varchar(32) NOT NULL,
   Weight        numeric(10, 2) NOT NULL, 
   Value         numeric(8, 2), 
   Volume        int4, 
   ShipID        int8, 
   EmployeeID    int8 NOT NULL,
   ExpeditionID  int8 NOT NULL,
-  MoonName      varchar(32), 
+  MoonName      varchar(32),
   PRIMARY KEY (ID));
 CREATE TABLE Moon (
   Name           varchar(32) NOT NULL, 
@@ -83,13 +83,22 @@ CREATE TABLE Material_Scrap (
   ScrapID      int8 NOT NULL, 
   PRIMARY KEY (MaterialName, 
   ScrapID));
+CREATE TABLE Scrap_Name (
+  Name  varchar(32) NOT NULL,
+  PRIMARY KEY (Name));
+CREATE TABLE Excavation_Event (
+  ExpeditionID  BIGINT,
+  EmployeeID    BIGINT,
+  EventCount    int2
+);
 ALTER TABLE Employee ADD CONSTRAINT FKEmployee772082 FOREIGN KEY (ShipID) REFERENCES Ship (ID);
 ALTER TABLE Scrap ADD CONSTRAINT FKScrap172778 FOREIGN KEY (ShipID) REFERENCES Ship (ID);
 ALTER TABLE Tools ADD CONSTRAINT FKTools894302 FOREIGN KEY (ShipID) REFERENCES Ship (ID);
 ALTER TABLE Expedition ADD CONSTRAINT FKExpedition353510 FOREIGN KEY (ShipID) REFERENCES Ship (ID);
 ALTER TABLE Expedition ADD CONSTRAINT FKExpedition210732 FOREIGN KEY (ScrapID) REFERENCES Scrap (ID);
 ALTER TABLE Scrap ADD CONSTRAINT FKScrap639576 FOREIGN KEY (EmployeeID) REFERENCES Employee (ID);
-ALTER TABLE Scrap ADD CONSTRAINT FKScrapExp420 FOreIGN KEY (ExpeditionID) REFERENCES Expedition (ID);
+ALTER TABLE Scrap ADD CONSTRAINT FKScrapExp420 FOREIGN KEY (ExpeditionID) REFERENCES Expedition (ID);
+ALTER TABLE Scrap ADD CONSTRAINT FKScrapName FOREIGN KEY (Name) REFERENCES Scrap_Name (Name);
 ALTER TABLE Ship_Engine ADD CONSTRAINT FKShip_engine806708 FOREIGN KEY (ShipID) REFERENCES Ship (ID);
 ALTER TABLE Employee ADD CONSTRAINT FKEmployee873402 FOREIGN KEY (Rank) REFERENCES Rank (RankName);
 ALTER TABLE Scrap ADD CONSTRAINT FKScrap514826 FOREIGN KEY (MoonName) REFERENCES Moon (Name);
--- changelog/dml/01populate.sql
+++ changelog/dml/01populate.sql
@@ -12,7 +12,7 @@ INSERT INTO rank (rankname, requiredscore) VALUES ('Boss', 500);
 
 INSERT INTO ship (id, maxload, maxfuel, maxvolume, load, fuel, volume, coordx, coordy, coordz) VALUES (default, 2000, 600, 5000, 200, 500, 2000, 21, 37, 60);
 INSERT INTO ship (id, maxload, maxfuel, maxvolume, load, fuel, volume, coordx, coordy, coordz) VALUES (default, 2000, 100, 5000, 100, 100, 2000, 21, 37, 60);
-INSERT INTO ship (id, maxload, maxfuel, maxvolume, load, fuel, volume, coordx, coordy, coordz) VALUES (default, 2000, 200, 5000, 400, 200, 2000, 21, 37, 60);
+INSERT INTO ship (id, maxload, maxfuel, maxvolume, load, fuel, volume, coordx, coordy, coordz) VALUES (default, 2000, 500, 5000, 400, 200, 2000, 21, 37, 60);
 INSERT INTO ship (id, maxload, maxfuel, maxvolume, load, fuel, volume, coordx, coordy, coordz) VALUES (default, 2000, 1000, 5000, 500, 1000, 2000, 21, 37, 60);
 INSERT INTO ship (id, maxload, maxfuel, maxvolume, load, fuel, volume, coordx, coordy, coordz) VALUES (default, 4000, 1000, 6000, 1000, 600, 1000, 2000, 3000, 4000);
 INSERT INTO ship (id, maxload, maxfuel, maxvolume, load, fuel, volume, coordx, coordy, coordz) VALUES (default, 1000, 1500, 700, 800, 200, 500, 1234, 8765, 9021);
@@ -22,7 +22,7 @@ INSERT INTO ship_engine (id, state, power, shipid) VALUES (default, 'Working', 3
 INSERT INTO ship_engine (id, state, power, shipid) VALUES (default, 'Working', 3, 1);
 INSERT INTO ship_engine (id, state, power, shipid) VALUES (default, 'Stopped', 9, 2);
 INSERT INTO ship_engine (id, state, power, shipid) VALUES (default, 'Working', 1, 3);
-INSERT INTO ship_engine (id, state, power, shipid) VALUES (default, 'Stopped', 1, 3);
+INSERT INTO ship_engine (id, state, power, shipid) VALUES (default, 'Working', 1, 3);
 INSERT INTO ship_engine (id, state, power, shipid) VALUES (default, 'Working', 3, 4);
 INSERT INTO ship_engine (id, state, power, shipid) VALUES (default, 'Working', 3, 5);
 INSERT INTO ship_engine (id, state, power, shipid) VALUES (default, 'Working', 3, 6);
@@ -31,10 +31,16 @@ INSERT INTO tools (id, name, durability, energy, shipid) VALUES (default, 'Wrenc
 INSERT INTO tools (id, name, durability, energy, shipid) VALUES (default, 'Ladder', 9, 0, 2);
 INSERT INTO tools (id, name, durability, energy, shipid) VALUES (default, 'Wrench', 3, 1, 2);
 
+
 INSERT INTO employee (id, name, surname, dateofbirth, joindate, heartrate, shipid, rank) VALUES (default, 'Maciej', 'Tomczuk', to_date('16-10-2001', 'DD-MM-YYYY'), to_date('20-12-2023', 'DD-MM-YYYY'), 120, 1, 'Intern');
 INSERT INTO employee (id, name, surname, dateofbirth, joindate, heartrate, shipid, rank) VALUES (default, 'Adam', 'Wysocki', to_date('4-5-2001', 'DD-MM-YYYY'), to_date('20-12-2023', 'DD-MM-YYYY'), 105, 1, 'Boss');
+
 INSERT INTO employee (id, name, surname, dateofbirth, joindate, heartrate, shipid, rank) VALUES (default, 'Bartosz', 'Słomowicz', to_date('12-5-2001', 'DD-MM-YYYY'), to_date('20-12-2023', 'DD-MM-YYYY'), 160, 2, 'Leader');
 INSERT INTO employee (id, name, surname, dateofbirth, joindate, heartrate, shipid, rank) VALUES (default, 'Szymon', 'Ludwiniak', to_date('15-12-2000', 'DD-MM-YYYY'), to_date('21-12-2023', 'DD-MM-YYYY'), 95, 2, 'Leader');
+
+INSERT INTO employee (id, name, surname, dateofbirth, joindate, heartrate, shipid, rank) VALUES (default, 'Arkadiusz', 'Pytka', to_date('12-5-2001', 'DD-MM-YYYY'), to_date('20-12-2023', 'DD-MM-YYYY'), 160, 3, 'Boss');
+INSERT INTO employee (id, name, surname, dateofbirth, joindate, heartrate, shipid, rank) VALUES (default, 'Dmitry', 'Goltstein', to_date('15-12-2000', 'DD-MM-YYYY'), to_date('21-12-2023', 'DD-MM-YYYY'), 95, 3, 'Boss');
+
 
 INSERT INTO material (name, density, value) VALUES ('Lead', 12, 0.2);
 INSERT INTO material (name, density, value) VALUES ('Gold', 14, 5);
@@ -43,16 +49,12 @@ INSERT INTO material (name, density, value) VALUES ('Aluminium', 3.5, 2);
 
 INSERT INTO moon (name, coordx, coordy, coordz, temperature, dangerlevel, weather) VALUES ('Sigma', 213, 973, 204, 25, 100, 'None');
 INSERT INTO moon (name, coordx, coordy, coordz, temperature, dangerlevel, weather) VALUES ('Tau', 1000, 1111, 60, 0, 5, 'Rainy');
+INSERT INTO moon (name, coordx, coordy, coordz, temperature, dangerlevel, weather) VALUES ('Beta', 21, 37, 60, 0, 5, 'None');
 
-INSERT INTO scrap (id, name, weight, value, volume, shipid, employeeid, moonname) VALUES (default, 'Heater', 40, null, 20, 1, 2, 'Sigma');
-INSERT INTO scrap (id, name, weight, value, volume, shipid, employeeid, moonname) VALUES (default, null, 10, null, 5, 2, 4, null);
-INSERT INTO scrap (id, name, weight, value, volume, shipid, employeeid, moonname) VALUES (default, 'Airhorn', 5, 100, 1, 2, 3, null);
-INSERT INTO scrap (id, name, weight, value, volume, shipid, employeeid, moonname) VALUES (default, 'Toaster', 15, 50, 30, 1, 1, 'Tau');
-
-INSERT INTO material_scrap (materialname, scrapid) VALUES ('Steel', 1);
-INSERT INTO material_scrap (materialname, scrapid) VALUES ('Steel', 3);
-INSERT INTO material_scrap (materialname, scrapid) VALUES ('Steel', 4);
-INSERT INTO material_scrap (materialname, scrapid) VALUES ('Lead', 1);
-INSERT INTO material_scrap (materialname, scrapid) VALUES ('Lead', 2);
-INSERT INTO material_scrap (materialname, scrapid) VALUES ('Gold', 2);
-INSERT INTO material_scrap (materialname, scrapid) VALUES ('Aluminium', 3);
+INSERT INTO Scrap_Name (name) VALUES ('Axle');
+INSERT INTO Scrap_Name (name) VALUES ('Apparatus');
+INSERT INTO Scrap_Name (name) VALUES ('Big bolt');
+INSERT INTO Scrap_Name (name) VALUES ('Bottles');
+INSERT INTO Scrap_Name (name) VALUES ('Brass bell');
+INSERT INTO Scrap_Name (name) VALUES ('Chemical jug');
+INSERT INTO Scrap_Name (name) VALUES ('Clown horn');
